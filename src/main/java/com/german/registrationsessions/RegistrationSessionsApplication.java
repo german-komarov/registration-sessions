a2df1c9affec6fea65fdeff85dafe5312f741a7c
german-komarov <german.komarov@ufaz.az>
2021-11-13 19:17:43 +0400

    commit-2

--- src/main/java/com/german/registrationsessions/RegistrationSessionsApplication.java
+++ src/main/java/com/german/registrationsessions/RegistrationSessionsApplication.java
@@ -2,9 +2,13 @@ package com.german.registrationsessions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @SpringBootApplication
 public class RegistrationSessionsApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(RegistrationSessionsApplication.class, args);
---
+++ src/main/java/com/german/registrationsessions/domain/User.java
@@ -0,0 +1,8 @@
+package com.german.registrationsessions.domain;
+
+
+import javax.persistence.Entity;
+
+@Entity // annotation that indicates that this class has representation in th db and should be mapped by JPA
+public class User {
+}
