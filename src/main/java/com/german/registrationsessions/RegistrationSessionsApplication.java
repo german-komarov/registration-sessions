f7942dbe3e5eb56b0da5134790c26436ec22ceaf
german-komarov <german.komarov@ufaz.az>
2021-11-13 06:59:57 +0400

    commit-1

---
+++ src/main/java/com/german/registrationsessions/RegistrationSessionsApplication.java
@@ -0,0 +1,13 @@
+package com.german.registrationsessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSessionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationSessionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/german/registrationsessions/RegistrationSessionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.german.registrationsessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSessionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
